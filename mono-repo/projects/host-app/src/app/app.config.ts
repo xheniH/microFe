149b105be88f86b871f6f9c9230cf515d1e1a418
Xheni Haxhiu <haxhiuxheni15@gmail.com>
2024-04-08 16:50:09 +0200

    oauth login

--- mono-repo/projects/host-app/src/app/app.config.ts
+++ mono-repo/projects/host-app/src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient(), importProvidersFrom(OAuthModule.forRoot())]
 };
--- mono-repo/projects/host-app/src/app/layout/header/header.component.ts
+++ mono-repo/projects/host-app/src/app/layout/header/header.component.ts
@@ -1,13 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { JwksValidationHandler, OAuthModule, OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from '../../sso.config';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  constructor(private oauthService: OAuthService) {
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn() {
+    this.oauthService.configure(authCodeFlowConfig);
+    // this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login() {
+    console.log('login', window.location.origin);
+    this.oauthService.initImplicitFlow();
+  }
+
+  logout() {
+    console.log('logout');
+    this.oauthService.logOut();
+  }
 }
