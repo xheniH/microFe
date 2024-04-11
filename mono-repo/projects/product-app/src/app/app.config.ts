acd1979c0bc01f055876e28aa76036e4d8061007
xheniH <xheni.haxhiu@codvo.ai>
2024-04-11 22:37:29 +0200

    login on microfrontends

--- mono-repo/projects/product-app/src/app/app.config.ts
+++ mono-repo/projects/product-app/src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAuth0({
+    domain: 'dev-jkx0w5qp0jdls8bn.us.auth0.com',
+    clientId: '5mM0SNPPnxcLLjiR4TAos0VQgXx3HuBQ',
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  })]
 };
